2e2da5774ebe14a6cad998ce1014f291e04d8415
cqx <chenqixing@115.com>
2011-09-07 17:21:28 +0800

    更改菜单重载方法

--- src/lib/ymenu/YMenu.py
+++ src/lib/ymenu/YMenu.py
@@ -476,32 +476,12 @@ def YMenu_factory(applet, iid):
 def reload_ymenu(monitor, thefile, arg, event):
         if event != gio.FILE_MONITOR_EVENT_CHANGED:
             return
-        object_name = "ymenu_screen0"
-        object_dir  = "/apps/panel/applets/"
-        object_client = gconf.client_get_default()
-        appletidlist = object_client.get_list("/apps/panel/general/applet_id_list", "string")
-        for applet in appletidlist:
-            bonobo_id = object_client.get_string("/apps/panel/applets/" + applet + "/bonobo_iid")
-            panel_position = object_client.get_int("/apps/panel/applets/" + applet + "/position")
-            if bonobo_id == "OAFIID:GNOME_YMenu":
-                panel = object_client.get_string("/apps/panel/applets/" + applet + "/toplevel_id")
-                appletidlist.remove(applet)
-                object_client.set_list("/apps/panel/general/applet_id_list", gconf.VALUE_STRING, appletidlist)
-                os.system("sleep 1.5")
+	os.system("/bin/sh -c '%sYMenu_Reload.py' &" % Globals.ProgramDirectory)
+	Globals.ReloadSettings()
 
-                object_client.set_string(object_dir + object_name +"/"+ "action_type", "lock")
-                object_client.set_bool(object_dir + object_name +"/"+ "locked", True)
-                object_client.set_int(object_dir + object_name +"/"+ "position", 0)
-                object_client.set_string(object_dir + object_name +"/"+ "toplevel_id", panel)
-                object_client.set_string(object_dir + object_name +"/"+ "object_type", "bonobo-applet")
-                object_client.set_string(object_dir + object_name +"/"+ "bonobo_iid", bonobo_id)
-
-                appletidlist.append(object_name)
-                object_client.set_list("/apps/panel/general/applet_id_list", gconf.VALUE_STRING, appletidlist)
-	gtk.main_quit()
 
 import gio
-dir_monitor = gio.File("/usr/lib/ymenu/YMenu.py").monitor(gio.FILE_MONITOR_NONE, None)
+dir_monitor = gio.File("%sYMenu.py" % Globals.ProgramDirectory).monitor(gio.FILE_MONITOR_NONE, None)
 dir_monitor.connect("changed", reload_ymenu)
 
 if len(sys.argv) == 2:
---
+++ src/lib/ymenu/YMenu_Reload.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import gconf
+import os
+object_name = "ymenu_screen0"
+object_dir  = "/apps/panel/applets/"
+object_client = gconf.client_get_default()
+appletidlist = object_client.get_list("/apps/panel/general/applet_id_list", "string")
+for applet in appletidlist:
+    bonobo_id = object_client.get_string("/apps/panel/applets/" + applet + "/bonobo_iid")
+    panel_position = object_client.get_int("/apps/panel/applets/" + applet + "/position")
+    if bonobo_id == "OAFIID:GNOME_YMenu":
+        panel = object_client.get_string("/apps/panel/applets/" + applet + "/toplevel_id")
+        appletidlist.remove(applet)
+        object_client.set_list("/apps/panel/general/applet_id_list", gconf.VALUE_STRING, appletidlist)
+        os.system("sleep 0.3")
+
+        object_client.set_string(object_dir + object_name +"/"+ "action_type", "lock")
+        object_client.set_bool(object_dir + object_name +"/"+ "locked", True)
+        object_client.set_int(object_dir + object_name +"/"+ "position", 0)
+        object_client.set_string(object_dir + object_name +"/"+ "toplevel_id", panel)
+        object_client.set_string(object_dir + object_name +"/"+ "object_type", "bonobo-applet")
+        object_client.set_string(object_dir + object_name +"/"+ "bonobo_iid", bonobo_id)
+
+        appletidlist.append(object_name)
+        object_client.set_list("/apps/panel/general/applet_id_list", gconf.VALUE_STRING, appletidlist)
+os.system("sleep 0.3")
